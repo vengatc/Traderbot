640156df841125ef2cd60e7b583bfeb90aecc6fd
vengatc <vengateswaran.c@gmail.com>
2016-04-21 20:07:19 -0700

    test data load is removed from the Trader.

--- mitty/src/main/java/mitty/api/BotAPI.java
+++ mitty/src/main/java/mitty/api/BotAPI.java
@@ -4,8 +4,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import mitty.asset.Assets;
 import mitty.bot.Bot;
+import mitty.market.MarketTicker;
+import mitty.statergy.ProfitBooker;
 import mitty.statergy.StatergyManager;
 import mitty.statergy.TradeStatergy;
+import mitty.statergy.UpHill;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,20 +21,50 @@ public class BotAPI {
 	public String tradeDecisions() {
 		String status = Assets.instance().toString();
 		status += "STATERGY {\n";
-		return status + Bot.instance().tradeDecisions() +"}\n";
+		return status + Bot.instance().tradeDecisions() + "}\n";
 	}
 
-	
+	// setting the statergy for the symbol.
 	@RequestMapping("addstatergy/{symbol}/{statergy}")
-	public synchronized boolean addStatergy(@PathVariable String symbol, @PathVariable String statergy) {
-		
-		TradeStatergy statergyinstance= StatergyManager.createStatergy(symbol,statergy);
-		
-		if(statergyinstance!=null){
+	public boolean addStatergy(@PathVariable String symbol, @PathVariable String statergy) {
+
+		TradeStatergy statergyinstance = StatergyManager.createStatergy(symbol, statergy);
+
+		if (statergyinstance != null) {
 			Bot.instance().addStatergy(statergyinstance);
 			return true;
 		}
 		return false;
 	}
-	
+
+	@RequestMapping("loadtestdata")
+	public void loadSampleData() {
+		Bot bot = Bot.instance();
+		if (Assets.instance().getTotalValue() == 0) {
+			System.out.println("Doing seed funding");
+			// seed portfolio once;
+			Assets.instance().getMoneyMarket().deposit(10000);
+			String stock = "TSLA";
+			double price = MarketTicker.instance().getQuote(stock);
+			int number = (int) (5000 / price);
+			Assets.instance().getPortfolio().buy(stock, number);
+
+			stock = "GLD";
+			price = MarketTicker.instance().getQuote(stock);
+			number = (int) (5000 / price);
+			Assets.instance().getPortfolio().buy(stock, number);
+
+			stock = "TSLA";
+			UpHill uphillstatergy = new UpHill(1, stock, 0);
+			bot.addStatergy(uphillstatergy);
+			ProfitBooker profitbooker = new ProfitBooker(1, stock);
+			bot.addStatergy(profitbooker);
+
+			stock = "GLD";
+			uphillstatergy = new UpHill(1, stock, 0);
+			bot.addStatergy(uphillstatergy);
+		}
+
+	}
+
 }
--- mitty/src/main/java/mitty/bot/Trader.java
+++ mitty/src/main/java/mitty/bot/Trader.java
@@ -9,41 +9,15 @@ import mitty.statergy.UpHill;
 
 public class Trader {
 	public static void start() {
-	 Bot bot = Bot.instance();
+		Bot bot = Bot.instance();
 		for (TradeStatergy statergy : StatergyManager.getAllStatergy()) {
 			bot.addStatergy(statergy);
 		}
 
-	  System.out.println ("Assets at start");
-	  System.out.println ( Assets.instance().toString());
-	  System.out.println("-------------------------------");
-	  if(Assets.instance().getTotalValue()==0){    
-	  System.out.println("Doing seed funding");	  
-	  //seed portfolio once;	  
-      Assets.instance().getMoneyMarket().deposit(10000);
-      String stock = "TSLA";
-      double price = MarketTicker.instance().getQuote(stock);
-      int number = (int) (5000/price);
-      Assets.instance().getPortfolio().buy(stock, number);
+		System.out.println("Assets at start");
+		System.out.println(Assets.instance().toString());
+		System.out.println("-------------------------------");
+		bot.start();
 
-      stock = "GLD";
-      price = MarketTicker.instance().getQuote(stock);
-      number = (int) (5000/price);
-      Assets.instance().getPortfolio().buy(stock, number);
-      
-  	  stock = "TSLA";
-      UpHill uphillstatergy = new UpHill(1, stock,0);
-      bot.addStatergy(uphillstatergy);
-      ProfitBooker profitbooker = new ProfitBooker(1, stock);
-      bot.addStatergy(profitbooker);
-
-      stock = "GLD";
-      uphillstatergy = new UpHill(1, stock,0);
-      bot.addStatergy(uphillstatergy);
-	  }
-	  
-      
-      bot.start();
-      
 	}
 }
