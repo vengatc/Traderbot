a8382110964bf5ddbcaacf56c73741b17b1e67ef
vengatc <vengateswaran.chandrasekaran@sap.com>
2017-05-27 11:45:54 -0700

    buffet buy business

---
+++ mitty/src/main/java/mitty/buffet/buybusiness/Business.java
@@ -0,0 +1,316 @@
+package mitty.buffetvalue.business;
+
+import java.util.Arrays;
+import java.util.Date;
+
+/**
+ * Created by i839511 on 5/21/17.
+ */
+public class Business implements Comparable<Business>{
+
+
+
+
+    String ticker;
+    double marketPrice;
+    double bookValue; //book Value per share.
+    double pe; //pricebyEarnings
+    double debtToEquity;
+    double fiveYearBookValueGrowth;
+    double tenYearFedRate;
+    double divident;
+    double tenBookValueGrowth;
+    double currentRatio; //Money comming in / money going out per 12 months. totalCurrentAssets ( likable to convert to cash) / total current liablity (going to pay out in next 12 months)
+    double eps;
+    double roe; //return on equity.
+    double returnOnCurrentPrice;
+
+    public double getEps() {
+        return eps;
+    }
+
+    public void setEps(double eps) {
+        this.eps = eps;
+    }
+
+
+    public double getRoe() {
+        return roe;
+    }
+
+    public void setRoe(double roe) {
+        this.roe = roe;
+    }
+    public Business(String ticker) {
+        this.ticker = ticker;
+     }
+
+    public static void main(String arg[]) {
+
+        String[] portfolio = {"SAP","BRK.B","WMT","TGT","WFC","AAPL","BAC","GS","AMAT","MSFT","GOOG","NVDA","COST"};
+
+        // List<Business> businesss = Arrays.stream(portfolio).map(ticker->BusinessFactory.businessFactory(ticker)).collect(Collectors.toList());//.forEach(
+        // System.out.println("SSSS");
+        //        bus-> bus.printValuation()
+        //);
+
+
+        System.out.println("Date :"+ new Date().toString());
+        Arrays.stream(portfolio).map(BusinessFactory.me).filter(bus -> bus.pe != 0).forEach(bus -> {
+            bus.printValuation();
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+            }
+        });
+
+      /*  Business wfc = new Business("WFC",53.06,13.26,35.22);
+        wfc.printValuation();
+
+        Business apple = new Business("AAPL",153.06,17.83 ,25.72);
+        apple.printValuation();
+
+        Business bac = new Business("BAC",23.05,14.14 ,24.41);
+        bac.printValuation();
+
+        Business gs = new Business("GS",215.39,11.48  ,192.35);
+        gs.printValuation();*/
+    }
+
+    public void setTenYearFedRate(double tenYearFedRate) {
+        this.tenYearFedRate = tenYearFedRate;
+    }
+
+    public void setFiveYearBookValueGrowth(double fiveYearFedRate) {
+        fiveYearBookValueGrowth = fiveYearFedRate;
+    }
+
+    public void setDivident(double divident) {
+        this.divident = divident;
+    }
+
+    public void setTenBookValueGrowth(double tenBookValueGrowth) {
+        this.tenBookValueGrowth = tenBookValueGrowth;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public void setMarketPrice(double marketPrice) {
+        this.marketPrice = marketPrice;
+    }
+
+    public void setBookValue(double bookValue) {
+        this.bookValue = bookValue;
+    }
+
+    public void setPe(double pe) {
+        this.pe = pe;
+    }
+
+    /**
+     * Buffet Rules.
+     * 1. Stock must be stable and understandable.
+     * 2. A stock must have long term prospect.
+     * 3. Much be managed by vigilent leders.
+     * 4. Stock must be undervalued. ( Low PE <15 and Price/Book (Safety net) <1.5
+     * <p>
+     * <p>
+     * Buy in such that stock market can close, and not open for 5  years.
+     * <p>
+     * Price is what yo8u pay, value is what you get.
+     */
+
+
+    double quickValuation() {
+        double valueation = pe * KeyNumbers.priceByBookValue(bookValue, marketPrice);
+        return valueation;
+    }
+
+    boolean quickValuationTool() {
+
+        return quickValuation() < 22.5;
+    }
+
+    double investmentSafetyPercentage() {
+        return KeyNumbers.InvestmentSafetyPercentage(bookValue, marketPrice);
+    }
+
+    double interestPercentage() {
+
+        return KeyNumbers.interestPercentage(pe);
+    }
+
+    void printValuation() {
+        System.out.println("______________________" + getTicker() + "______________________");
+        System.out.printf("Price (%.2f) P/E(%.2f) BookValue(%.2f) Debt2Equity(%.2f) CurrentRatio(%.2f) TenYearGrowthBV(%.2f) FiveYearGrowthBV(%.2f) Divident(%.2f)  FedRate(%.2f) ROE(%.2f) \n", marketPrice, pe, bookValue, debtToEquity, currentRatio, tenBookValueGrowth, fiveYearBookValueGrowth, divident, tenYearFedRate,roe);
+
+        System.out.printf("valuation(<22.5) =  %.2f \n", quickValuation());
+        System.out.println(getTicker() + "passing quick valuation = " + quickValuationTool());
+        System.out.printf(getTicker() + " Investment safety percentage = %.2f\n", investmentSafetyPercentage());
+        System.out.printf(getTicker() + " Interest percentage = %.2f \n", interestPercentage());
+        System.out.printf(getTicker() + " Vigilant leadership check = " + vigilantLeaderShip() + "\n");
+        System.out.printf(getTicker() + " Intrinic Value (10 Year book value avg)  of Stock= %.2f \n", intrinicValue());
+        System.out.printf(getTicker() + " Intrinic Value (5 Year book value avg)  of Stock= %.2f \n", intrinic5YearBookValue());
+
+        System.out.printf(getTicker() + " Interest rate if bought on current market price = %.2f \n", percentageForCurrentPrice());
+
+    }
+
+    /**
+     * Rule 1.Vigialt leadership
+     *
+     * @return
+     */
+    boolean vigilantLeaderShip() {
+        return (getDebtToEquity() < 0.5 && getCurrentRatio() > 1.5);
+    }
+
+    /**
+     * Risk associated with owning  Investment.
+     *
+     * 1. Excessive Debt owned by company(only when tide goes out you will see who is swiming naked)
+     * 2. Overpaying for an investment (Price is what you pay , value is what you get)
+     * 3. Not knowing what you are doing.
+     *
+     * //Compare value with federal note.
+     *
+     * S&P rating anything with A.
+     * Municipal can be B.
+     * FED interest curve
+     *
+     * @param arg
+     */
+
+
+    /***
+     * Four Rules for INVESTING
+     *
+     * 1) Managed by viginalat leader (look aout for possible danger
+     *    (DEBT) => DEBT/EQUIT < 0.5 by waren.
+     * 2) Long term prospect
+     * 3) Stock must by stable and understandable.
+     * 4) Stock must be undervalued.
+     *
+     * @param arg
+     */
+
+    /**
+     * 1) Managed by viginalat leader (look aout for possible danger
+     * (DEBT) => DEBT/EQUIT < 0.5 by waren likes it.
+     *
+     * @return
+     */
+    public double getDebtToEquity() {
+        return debtToEquity;
+    }
+
+    public void setDebtToEquity(double debtToEquity) {
+        this.debtToEquity = debtToEquity;
+    }
+
+    /**
+     * 1) Managed by viginalat leader
+     * Current Ratio > 1.5 by waren likes it.
+     * <p>
+     * How company will handle debt in next 12 months.
+     * Money in in 12 months and Money to pay out in next 12 months.
+     * <p>
+     * Money comming in / money going out per 12 months.
+     * totalCurrentAssets ( likable to convert to cash)
+     * / total current liablity (going to pay out in next 12 months)
+     *
+     * @return
+     */
+    public double getCurrentRatio() {
+        return debtToEquity;
+    }
+
+
+    /*public double interinsicValueCalculator(){
+
+    }*/
+
+    public void setCurrentRatio(double currentRatio) {
+        this.currentRatio = currentRatio;
+    }
+
+    double intrinicValue() {
+        return intrinicValue(tenYearFedRate, tenBookValueGrowth);
+    }
+
+    double intrinic5YearBookValue() {
+        return intrinicValue(tenYearFedRate, fiveYearBookValueGrowth);
+    }
+
+    /**
+     * Purchase price of the stock to get Fed interest rate.
+     *
+     * @return
+     */
+    double intrinicValue(double tenYearRate, double bookValueGrowth) {
+        double currentdivident = divident;
+        double gain = bookValueGrowth;
+        double currentBookValue = bookValue;
+
+        // coupon=Number(document.calculator.coupon.value);
+        // par=Number(document.calculator.par.value);
+        double year = 10; // number of years we are comparing agains.
+
+        double TenYearFedRater = tenYearRate;
+
+        double perc = (1 + gain / 100);
+        double base = Math.pow(perc, year);
+        double parr = currentBookValue * base;
+        TenYearFedRater = TenYearFedRater / 100;
+
+        double extra = Math.pow((1 + TenYearFedRater), year);
+
+        double c = currentdivident * (1 - (1 / extra)) / TenYearFedRater + parr / extra;
+        return c;
+    }
+
+
+    /*double gain(){
+
+        double currentBookValue = bookValue;
+        double oldBookValue = oldBookValue;
+        double noOfYears = 3;
+        double upper = 1/noOfYears;
+        double base = currentBookValue/oldBookValue;
+        double m= Math.pow(base,upper);
+        double c = 100*(m-1);
+        return c;
+
+    }
+*/
+
+    /**
+     * Perentage (interest) return per year , based on current market price.
+     *
+     * @return
+     */
+    double percentageForCurrentPrice() {
+        double i = 0.01;
+        double r = 0.5;
+        double iVal = Math.floor(intrinicValue(r, tenBookValueGrowth));
+        double mPrice = Math.floor(marketPrice);
+        while (iVal != mPrice) {
+            if (iVal > mPrice) r = r + i;
+            if (iVal < mPrice) r = r - i;
+            iVal = Math.floor(intrinicValue(r, tenBookValueGrowth));
+            //System.out.println(iVal+":"+r);
+        }
+        return r;
+    }
+
+    @Override
+    public int compareTo(Business o) {
+
+    }
+}
---
+++ mitty/src/main/java/mitty/buffet/buybusiness/BusinessFactory.java
@@ -0,0 +1,402 @@
+package mitty.buffetvalue.business;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.function.Function;
+
+/**
+ * Created by i839511 on 5/21/17.
+ * Fetch and create business.
+ */
+public class BusinessFactory implements Function<String, Business> {
+
+    static double  TenYearFedRate=get10YearFedRate();
+
+    public static BusinessFactory me = new BusinessFactory();
+
+    static double getMarketPrice(String ticker) {
+        try {
+            String url = "http://www.msn.com/en-us/money/stockdetails?symbol=$$";
+            url = url.replace("$$", ticker);
+
+            Document doc = Jsoup.connect(url).get();
+
+            Elements newsHeadlines = doc.getElementsByClass("currentval");
+            return Double.parseDouble(newsHeadlines.text());
+        } catch (Exception e) {
+            //e.printStackTrace();
+            return 0;
+        }
+
+    }
+
+    static double getPE(String ticker) {
+        //  <li class="today-trading-tile">
+        //<span class="name"><p class='truncated-string'  title='P/E Ratio (EPS)'>P/E Ratio (EPS)</p></span>
+        // <span class="value baseminus"><p class='truncated-string'  title='11.48 (18.76) '>11.48 (18.76) </p></span>
+        // </li>
+
+        try {
+            String url = "http://www.msn.com/en-us/money/stockdetails?symbol=$$";
+            url = url.replace("$$", ticker);
+
+            Document doc = Jsoup.connect(url).get();
+
+
+            Elements p = doc.select("p.truncated-string");
+
+            boolean found = false;
+            String peString = "";
+            for (Element n : p) {
+                if (found) {
+                    peString = n.text();
+                    break;
+                }
+                if (n.text().contains("P/E Ratio (EPS)")) {
+                    found = true;
+                }
+            }
+
+            return Double.parseDouble(peString.split(" ")[0]);
+        } catch (Exception e) {
+            //e.printStackTrace();
+            return 0;
+        }
+
+    }
+
+    static double getDebtToEquityRatio(String ticker) {
+        //  <li class="today-trading-tile">
+        //<span class="name"><p class='truncated-string'  title='P/E Ratio (EPS)'>P/E Ratio (EPS)</p></span>
+        // <span class="value baseminus"><p class='truncated-string'  title='11.48 (18.76) '>11.48 (18.76) </p></span>
+        // </li>
+
+        try {
+            String url = "http://www.msn.com/en-us/money/stockdetails/analysis?symbol=$$";
+            url = url.replace("$$", ticker);
+
+            Document doc = Jsoup.connect(url).get();
+
+
+            Elements p = doc.select("p.truncated-string");
+
+            boolean found = false;
+            String peString = "";
+            for (Element n : p) {
+                if (found) {
+                    peString = n.text();
+                    break;
+                }
+                if (n.text().contains("Debt/Equity Ratio")) {
+                    found = true;
+                }
+            }
+
+            return Double.parseDouble(peString);
+        } catch (Exception e) {
+           // e.printStackTrace();
+            return 0;
+        }
+
+    }
+
+
+    static double getRoe(String ticker) {
+        //  <li class="today-trading-tile">
+        //<span class="name"><p class='truncated-string'  title='P/E Ratio (EPS)'>P/E Ratio (EPS)</p></span>
+        // <span class="value baseminus"><p class='truncated-string'  title='11.48 (18.76) '>11.48 (18.76) </p></span>
+        // </li>
+
+        try {
+            String url = "http://www.msn.com/en-us/money/stockdetails/analysis?symbol=$$";
+            url = url.replace("$$", ticker);
+
+            Document doc = Jsoup.connect(url).get();
+
+
+            Elements p = doc.select("p.truncated-string");
+
+            boolean found = false;
+            String peString = "";
+            for (Element n : p) {
+                if (found) {
+                    peString = n.text();
+                    break;
+                }
+                if (n.text().contains("Return on Equity %")) {
+                    found = true;
+                }
+            }
+
+            return Double.parseDouble(peString);
+        } catch (Exception e) {
+            // e.printStackTrace();
+            return 0;
+        }
+
+    }
+
+
+    static double getCurrentRatio(String ticker) {
+        //  <li class="today-trading-tile">
+        //<span class="name"><p class='truncated-string'  title='P/E Ratio (EPS)'>P/E Ratio (EPS)</p></span>
+        // <span class="value baseminus"><p class='truncated-string'  title='11.48 (18.76) '>11.48 (18.76) </p></span>
+        // </li>
+
+        try {
+            String url = "http://www.msn.com/en-us/money/stockdetails/analysis?symbol=$$";
+            url = url.replace("$$", ticker);
+
+            Document doc = Jsoup.connect(url).get();
+
+
+            Elements p = doc.select("p.truncated-string");
+
+            boolean found = false;
+            String peString = "";
+            for (Element n : p) {
+                if (found) {
+                    peString = n.text();
+                    break;
+                }
+                if (n.text().contains("Current Ratio")) {
+                    found = true;
+                }
+            }
+
+            return Double.parseDouble(peString);
+        } catch (Exception e) {
+            //e.printStackTrace();
+            return 0;
+        }
+
+    }
+    static double getBookValue(String ticker) {
+        //<li style="width: 60%; " class="left-align  heading" data-role="">
+        //<p class='truncated-string'  title='Book Value/Share'>Book Value/Share</p>
+        // </li>
+        //<li style="width: 40%; " class="right-align  value" data-role="">
+        //<p class='truncated-string'  title='192.35'>192.35</p>
+        try {
+
+           // String url = "http://www.msn.com/en-us/money/stockdetails/analysis/fi-126.1.$$.NYS";
+             String url = "http://www.msn.com/en-us/money/stockdetails/analysis?symbol=$$";
+
+            url = url.replace("$$", ticker);
+
+            Document doc = Jsoup.connect(url).get();
+
+            Elements p = doc.select("p.truncated-string");
+
+            boolean found = false;
+            String peString = "";
+            for (Element n : p) {
+                if (found) {
+                    peString = n.text();
+                    break;
+                }
+                if (n.text().contains("Book Value/Share")) {
+                    found = true;
+                }
+            }
+
+            return Double.parseDouble(peString);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+
+    }
+
+
+    static double getCurrentDivident(String ticker) {
+        //<li style="width: 60%; " class="left-align  heading" data-role="">
+        //<p class='truncated-string'  title='Book Value/Share'>Book Value/Share</p>
+        // </li>
+        //<li style="width: 40%; " class="right-align  value" data-role="">
+        //<p class='truncated-string'  title='192.35'>192.35</p>
+        try {
+
+            String url = "http://www.msn.com/en-us/money/stockdetails/fi-126.1.$$.NYS?symbol=$$";
+            url = url.replace("$$", ticker);
+
+            Document doc = Jsoup.connect(url).get();
+
+            Elements p = doc.select("p.truncated-string");
+
+            boolean found = false;
+            String peString = "";
+            for (Element n : p) {
+                if (found) {
+                    peString = n.text();
+                    break;
+                }
+                if (n.text().contains("Dividend Rate (Yield)")) {
+                    found = true;
+                }
+            }
+
+            return Double.parseDouble(peString.split(" ")[0]);
+        } catch (Exception e) {
+            //e.printStackTrace();
+            return 0;
+        }
+
+    }
+    static double getAverage10YearBookValueGrowthPercentage(String ticker) {
+           //<tr>
+             //       <td><a href='/term/Book Value Per Share/NYSE:GS/Book%2BValue%2BPer%2BShare/Goldman%2BSachs%2BGroup%2BInc' class='nav' target='_blank'>Book Value Growth (%)</a></td>
+              //      <td>8.10&nbsp;</td>
+               //     <td>6.70&nbsp;</td>
+                //    <td>6.20&nbsp;</td>
+        try {
+
+            String url = "https://www.gurufocus.com/financials/$$";
+            url = url.replace("$$", ticker);
+
+            Document doc = Jsoup.connect(url).get();
+
+            Element table = doc.select("table[id=R1]").get(0);
+
+            Elements rows = table.select("tr");
+
+            for(Element row : rows){
+                Elements cols = row.select("td");
+
+                if(cols.size()>0 && cols.get(0).select("a").text().contains("Book Value Growth")){
+                    String tenyarstr= cols.get(1).text().replace("\u00a0", "");
+                   // System.out.println(tenyarstr.trim() + "HHHHHH");
+                    return Double.parseDouble(tenyarstr.trim());
+
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+       return 0;
+    }
+
+    static double getAverage5YearBookValueGrowthPercentage(String ticker) {
+        //<tr>
+        //       <td><a href='/term/Book Value Per Share/NYSE:GS/Book%2BValue%2BPer%2BShare/Goldman%2BSachs%2BGroup%2BInc' class='nav' target='_blank'>Book Value Growth (%)</a></td>
+        //      <td>8.10&nbsp;</td>
+        //     <td>6.70&nbsp;</td>
+        //    <td>6.20&nbsp;</td>
+        try {
+
+            String url = "https://www.gurufocus.com/financials/$$";
+            url = url.replace("$$", ticker);
+
+            Document doc = Jsoup.connect(url).get();
+
+            Element table = doc.select("table[id=R1]").get(0);
+
+            Elements rows = table.select("tr");
+
+            for(Element row : rows){
+                Elements cols = row.select("td");
+
+                if(cols.size()>0 && cols.get(0).select("a").text().contains("Book Value Growth")){
+                    String fiveyarstr= cols.get(2).text().replace("\u00a0", "");
+                    // System.out.println(tenyarstr.trim() + "HHHHHH");
+                    return Double.parseDouble(fiveyarstr.trim());
+
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+        return 0;
+    }
+
+    static double get10YearFedRate() {
+      // <table class="t-chart" xmlns:fmt="urn:treasury-xslt-fmt" xmlns:msxml="urn:schemas-microsoft-com:xslt" xmlns:d="http://schemas.microsoft.com/ado/2007/08/dataservices" xmlns:m="http://schemas.microsoft.com/ado/2007/08/dataservices/metadata" xmlns:a="http://www.w3.org/2005/Atom">
+        // <tr><th scope="col">Date</th><th scope="col">1 mo</th><th scope="col">3 mo</th><th scope="col">6 mo</th><th scope="col">1 yr</th><th scope="col">2 yr</th><th scope="col">3 yr</th><th scope="col">5 yr</th><th scope="col">7 yr</th><th scope="col">10 yr</th><th scope="col">20 yr</th><th scope="col">30 yr</th></tr>
+        // <tr class="oddrow">
+        // <td scope="row" class="text_view_data">05/01/17</td>
+        // <td class="text_view_data">0.67</td>
+        // <td class="text_view_data">0.83</td>
+        // <td class="text_view_data">0.98</td>
+        // <td class="text_view_data">1.09</td>
+        // <td class="text_view_data">1.28</td>
+        // <td class="text_view_data">1.48</td>
+        // <td class="text_view_data">1.84</td>
+        // <td class="text_view_data">2.13</td>
+        // <td class="text_view_data">2.33</td>
+        // <td class="text_view_data">2.71</td><td class="text_view_data">3.00</td></tr><tr class="evenrow"><td scope="row" class="text_view_data">05/02/17</td><td class="text_view_data">0.72</td><td class="text_view_data">0.82</td><td class="text_view_data">0.99</td><td class="text_view_data">1.08</td><td class="text_view_data">1.27</td><td class="text_view_data">1.45</td><td class="text_view_data">1.81</td><td class="text_view_data">2.09</td><td class="text_view_data">2.29</td><td class="text_view_data">2.68</td><td class="text_view_data">2.97</td></tr>
+        try {
+
+            String url = "https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=yield";
+
+            Document doc = Jsoup.connect(url).get();
+
+            Element table = doc.select("table[class=t-chart]").get(0);
+
+            Elements rows = table.select("tr");
+
+            for(Element row : rows){
+                Elements cols = row.select("td");
+
+                if(cols.size()>0 ){
+                    String tenyarstr= cols.get(9).text();
+                    // System.out.println(tenyarstr.trim() + "HHHHHH");
+                    return Double.parseDouble(tenyarstr.trim());
+
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+        return 0;
+    }
+    public static void main(String[] args) throws Exception {
+        //String url = "http://www.msn.com/en-us/money/stockdetails/fi-126.1.$$.NYS?symbol=$$";
+        //url = url.replace("$$", "GS");
+        //Document doc = Jsoup.connect(url).get();
+
+        //double bookVAlueGrowth = getAverage10YearBookValueGrowthPercentage("GS");
+       // double divident =  get10YearFedRate();
+                // getCurrentDivident("GS");
+
+
+       // System.out.println(bookVAlueGrowth);
+
+        System.out.println(getRoe("SAP"));
+
+    }
+
+    public Business apply(String ticker) {
+        double bookVAlueGrowth = getAverage10YearBookValueGrowthPercentage(ticker);
+        double marketPrice = getMarketPrice(ticker);
+        double pe = getPE(ticker);
+        double bookv = getBookValue(ticker);
+        double debtToEquity = getDebtToEquityRatio(ticker);
+        double currentRatio = getCurrentRatio(ticker);
+        double TenYearBookValue = getAverage10YearBookValueGrowthPercentage(ticker);
+        double FiveYearBookValue = getAverage5YearBookValueGrowthPercentage(ticker);
+        double divident = getCurrentDivident(ticker);
+        double roe = getRoe(ticker);
+
+        Business b = new Business(ticker);
+        b.setMarketPrice(marketPrice);
+        b.setPe(pe);
+        b.setBookValue(bookv);
+        b.setDebtToEquity(debtToEquity);
+        b.setCurrentRatio(currentRatio);
+        b.setTenBookValueGrowth(TenYearBookValue);
+        b.setDivident(divident);
+        b.setTenYearFedRate(TenYearFedRate);
+        b.setFiveYearBookValueGrowth(FiveYearBookValue);
+        b.setRoe(roe);
+        return b;
+    }
+}
